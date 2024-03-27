e4908adcb5f12938ad0c22ec09b3b042dc3dfe86
Sukanta Roy <78083659+itzFelu@users.noreply.github.com>
2024-03-28 02:07:05 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
-async function mongoConnect(url){
-    await mongoose.connect(url)
+async function mongoConnect(){
+    await mongoose.connect("mongodb+srv://itzfelu:<Harami...>@cluster0.zl9pdxe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         .then(()=>console.log("connected to mongoDB"))
         .catch((err)=>console.log("Mongo Error: ",err));
 }
