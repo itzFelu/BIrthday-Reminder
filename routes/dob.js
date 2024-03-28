4b87d6fdface6fcb1c00817ccbfd85a506ca4f25
itzFelu <phelusukanta11@gmail.com>
2024-03-28 14:21:24 +0530

    deleted all

--- routes/dob.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const {handleCreateEntry,handleDeleteEntry}=require('../controllers/home');
-const router = express.Router();
-
-router.post('/',handleCreateEntry);
-router.get('/:id',handleDeleteEntry);
-
-module.exports=router;
